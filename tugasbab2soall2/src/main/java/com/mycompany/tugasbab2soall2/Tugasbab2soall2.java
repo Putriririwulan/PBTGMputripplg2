91f538fcad3e76d9791968c0d8b3075e98666223
Putriririwulan <145527749+Putriririwulan@users.noreply.github.com>
2024-08-26 01:27:16 -0700

    Add files via upload

---
+++ tugasbab2soall2/src/main/java/com/mycompany/tugasbab2soall2/Tugasbab2soall2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbab2soall2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Tugasbab2soall2 {
+ public static String Makanan;
+//Membuat variable tanpa Value/Nilai pada class staticvariable 
+        public static final String Minuman = "Jus jambu";
+    public static void main(String[] args) {
+        
+ // TODO code application logic here
+ Makanan = "Ayam geprek ";//Mengisi Value pada variable Hobi
+ System.out.println("Makanan Favorit saya " + Makanan);//Memanggil Variable Hobi
+ System.out.println("Minuman Favorit saya " +
+Minuman);//Memanggil Variable Hewan
+ }
+}
+    
+
