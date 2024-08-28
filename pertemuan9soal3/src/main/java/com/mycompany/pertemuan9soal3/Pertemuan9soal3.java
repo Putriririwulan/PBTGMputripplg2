484e4c247c920e11666fdd11412a0a75b2c869bc
Putriririwulan <145527749+Putriririwulan@users.noreply.github.com>
2024-08-27 23:11:11 -0700

    Add files via upload

---
+++ pertemuan9soal3/src/main/java/com/mycompany/pertemuan9soal3/Pertemuan9soal3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan9soal3;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pertemuan9soal3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int i = 1;
+        while (i < 6) {
+          System.out.println(i);
+          i++;
+}
+    }
+}
