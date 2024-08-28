65419b18d962128ea1ea7ffddde8b78bc2568bca
Putriririwulan <145527749+Putriririwulan@users.noreply.github.com>
2024-08-27 19:09:29 -0700

    Add files via upload

---
+++ pertemuan3soal1/src/main/java/com/mycompany/pertemuan3soal1/Pertemuan3soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3soal1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pertemuan3soal1 {
+
+    public static void main(String[] args) {
+        //TODO code.appl caation logic here
+        int a = 9;
+        float f = 899;
+        char ch = 'A';
+        boolean b = false;
+        String string = "hello world";
+        System.out.println("int Var1 " + a);
+        System.out.println("float Var2 " + f);
+        System.out.println ("char Var3 " + ch);
+        System.out.println ("boolean Var4 " + b);
+        System.out.println ("string Var5 " + string);
+    }
+}
