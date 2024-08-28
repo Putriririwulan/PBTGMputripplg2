341e5c858a6e707255dc4610fb6ea6e09e25efc8
Putriririwulan <145527749+Putriririwulan@users.noreply.github.com>
2024-08-27 21:04:57 -0700

    Add files via upload

---
+++ pertemuan4soal1/src/main/java/com/mycompany/pertemuan4soal1/Pertemuan4soal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4soal1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pertemuan4soal1 {
+
+    public static void main(String[] args) {
+        
+    int angka1 = 10;
+ int angka2 = 3;
+ double pembagian = (double)angka1 / angka2;
+System.out.println("Hasil pertambahan a+b = " +(angka1 + angka2) );
+System.out.println("Hasil perkurangan a-b = " +(angka1 - angka2) );
+System.out.println("Hasil perkalian a*b = " +(angka1 * angka2) );
+System.out.println("Hasil perkalian a/b =  " +pembagian);
+    }
+    }
+
